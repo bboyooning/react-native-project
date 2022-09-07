f47fafe62867affa1a12cf516ceb98d943cfb120
bboyooning <bboyooning@gmail.com>
2022-09-08 00:38:50 +0900

    add: AuthApp 틀 잡아주기

--- StateManagement/App.tsx
+++ StateManagement/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import AuthApp from './components/AuthApp';
 import rootReducer from './slices';
 
 const store = createStore(rootReducer);
 
 function App() {
-  return <Provider store={store}>{/*Todo 컴포넌트*/}</Provider>;
+  return (
+    <Provider store={store}>
+      <AuthApp />
+    </Provider>
+  );
 }
 
 export default App;
